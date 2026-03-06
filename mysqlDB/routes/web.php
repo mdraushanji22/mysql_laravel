ff919e2434d47009ed87b384f7695848a304a7b4
mdraushan <mdraushanji22@gmail.com>
2026-03-06 23:57:02 +0530

    Laravel-MySQL

--- mysqlDB/routes/web.php
+++ mysqlDB/routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('user', [UserController::class, 'users']);
+Route::get('studetns', [UserController::class, 'users']);
+
+
