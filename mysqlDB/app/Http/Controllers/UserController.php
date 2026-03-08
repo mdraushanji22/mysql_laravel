957b9fdb1f5b79ecc4180eabd89e30295c95e874
mdraushan <mdraushanji22@gmail.com>
2026-03-08 15:15:03 +0530

    API-FetchData

--- mysqlDB/app/Http/Controllers/UserController.php
+++ mysqlDB/app/Http/Controllers/UserController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller
 {
     function users()
     {
-        $user = DB::select('select * from user');
-        return view('user', ['users' => $user]);
+        $users = DB::select('select * from user');
+
+        $response = Http::get('https://jsonplaceholder.typicode.com/users/1');
+        $data = json_decode($response->body());
+
+        return view('user', ['users' => $users, 'data' => $data]);
     }
 }
--- mysqlDB/resources/views/user.blade.php
+++ mysqlDB/resources/views/user.blade.php
@@ -1,6 +1,6 @@
 <div>
     <h1>User List</h1>
-    <table border=border-collapse>
+    <table border="1" style="border-collapse:collapse;">
         <tr>
             <th>ID</th>
             <th>Name</th>
@@ -17,4 +17,31 @@
         </tr>
         @endforeach
     </table>
+    <h1>User Data From API</h1>
+    <ul>
+        <li>
+            <span>Name:</span><span><b>{{$data->name}}</b></span>
+        </li>
+        <li>
+            <span>Email:</span><span><b>{{$data->email}}</b></span>
+        </li>
+        <li>
+            <span>Phone:</span><span><b>{{$data->phone}}</b></span>
+        </li>
+        <li>
+            <span>Adress:</span><span><b> {{$data->address->street}},
+                    {{$data->address->suite}},
+                    {{$data->address->city}},
+                    {{$data->address->zipcode}}</b></span>
+        </li>
+        <li>
+            <span>Company:</span><span><b>
+                    {{$data->company->name}},
+                    {{$data->company->catchPhrase}},
+                    {{$data->company->bs}}</b></span>
+        </li>
+        <li>
+            <span>Website:</span><span><b>{{$data->website}}</b></span>
+        </li>
+    </ul>
 </div>
