29196613f3ab962b06bc7804aa01c9703df04882
mdraushan <mdraushanji22@gmail.com>
2026-03-09 23:40:03 +0530

    Laravel-MySQL

--- mysqlDB/resources/views/students.blade.php
+++ mysqlDB/resources/views/students.blade.php
@@ -7,6 +7,8 @@
             <th>Name</th>
             <th>Email</th>
             <th>Batch</th>
+            <th>Create Date</th>
+            <th>Updated Date</th>
         </tr>
         @foreach($data as $student)
         <tr>
@@ -14,6 +16,8 @@
             <td>{{$student->name}}</td>
             <td>{{$student->email}}</td>
             <td>{{$student->batch}}</td>
+            <td>{{$student->created_at}}</td>
+            <td>{{$student->updated_at}}</td>
         </tr>
         @endforeach
     </table>
