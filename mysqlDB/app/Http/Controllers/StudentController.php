1cff45feaf7b7aa78b5b510c3b56ac686364d96f
mdraushan <mdraushanji22@gmail.com>
2026-03-10 20:36:41 +0530

    Laravel-MySQL

--- mysqlDB/app/Http/Controllers/StudentController.php
+++ mysqlDB/app/Http/Controllers/StudentController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
-    function getStudents()
+    function getStudents(Request $request)
     {
-        return view("Add student");
-        $data = new \App\Models\Student;
-        echo $data->testFun();
-        $students = \App\Models\Student::all();
-        return view('students', ['data' => $students]);
+        return $request->input();
+        // $data = new \App\Models\Student;
+        // echo $data->testFun();
+        // $students = \App\Models\Student::all();
+        // return view('students', ['data' => $students]);
     }
 }
