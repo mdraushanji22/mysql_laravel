1f31cccc1d38cc4a3028c415437c8420146543f9
mdraushan <mdraushanji22@gmail.com>
2026-03-27 22:51:09 +0530

    Laravel-MySQL

--- mysqlDB/resources/views/edit.blade.php
+++ mysqlDB/resources/views/edit.blade.php
@@ -1,5 +1,8 @@
 <div>
     <h1>Update Student Data</h1>
+    <form action="search" method="get">
+        <input type="text" name="search" placeholder="Enter search name">
+    </form>
     <form action="{{ url('edit-student/'.$data->id) }}" method="POST">
         @csrf
         @method('PUT')
