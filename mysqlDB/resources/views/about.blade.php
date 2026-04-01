1e71148e82215e71599baab176cc7da77724a2fa
mdraushan <mdraushanji22@gmail.com>
2026-04-01 22:30:35 +0530

    Laravel-MySQL

--- mysqlDB/resources/views/about.blade.php
+++ mysqlDB/resources/views/about.blade.php
@@ -1,9 +1,43 @@
-<div>
-    @extends('layout.app')
-    @section('title', 'Home Page')
+<!DOCTYPE html>
+<html lang="en">
 
-    @section('content')
-    <h1>This is about page </h1>
-    <p>Hi, My name is Md Raushan JIlani</p>
-    @endsection
-</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <div class="heading">
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+                <li>
+                    <a href="">Login</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h1>This is About page</h1>
+            <h3>Sub Heading</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Provident reiciendis, architecto dolorem doloremque magni
+                modi possimus beatae a laboriosam voluptas vel ea unde
+                cupiditate facere et ullam excepturi! Doloribus, minima?
+            </p>
+        </div>
+        <div class="footer">
+            <p>Footer</p>
+        </div>
+    </div>
+</body>
+
+</html>
